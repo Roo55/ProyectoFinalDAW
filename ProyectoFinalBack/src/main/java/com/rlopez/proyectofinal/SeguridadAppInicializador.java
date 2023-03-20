f886a0c2f74f6a0cf188237fa22482ffbef36fe3
RodriCasa <rodrigolopez500@gmail.com>
2023-03-20 15:55:27 +0100

    [ADD] JWT Implementado (hay que hacer pruebas). Proyecto arranca y responde a peticiones

--- ProyectoFinalBack/src/main/java/com/rlopez/proyectofinal/SeguridadAppInicializador.java
+++
@@ -1,7 +0,0 @@
-package com.rlopez.proyectofinal;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SeguridadAppInicializador extends AbstractSecurityWebApplicationInitializer{
-
-}
